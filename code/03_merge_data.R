825f3c6e345e4028efa4b76a5836dded2b92a473
sanyuktaghai <sanyukta22@gmail.com>
2018-12-03 12:17:45 -0800

    Adding analysis for agricultural profit as dependent and education of household head as explanatory

--- code/03_merge_data.R
+++ code/03_merge_data.R
@@ -1,6 +1,19 @@
 
 
+#highest education per household
+
+
+#education of head of household
 educ_hh_head <- merge(x = hh_head_data, y = educ_data, by = c('nh','pid','clust'), all.x = TRUE )
 
+# Education of each household using head of household
+educ_head <- educ_hh_head %>%
+              select(nh, clust, highest_educ_level)
+
+# Merge aggricultural profit with education by household head 
+agri_educ <- merge(x = agg2_data, y = educ_head, by = c('nh','clust'), all.x = TRUE) %>%
+              select(nh, clust, agri1c, highest_educ_level)
+
+
 #merge two dataframes
-educ_hh_head_region <- merge(educ_hh_head, region_info_data, by=c("nh","clust"), all.x = TRUE)
+#educ_hh_head_region <- merge(educ_head, region_info_data, by=c("nh","clust"), all.x = TRUE)
---
+++ code/06_regression_analysis.R
@@ -0,0 +1,14 @@
+lm_agri_educ <- lm(agri1c~highest_educ_level, data = agri_educ)
+
+res_lm_agri_educ <- resid(lm_agri_educ)
+std_res_lm_agri_educ <- rstandard(lm_agri_educ)
+fit_res_lm_agri_educ <- fitted(lm_agri_educ)
+
+breaks <- pretty(range(std_res_lm_agri_educ), n = nclass.FD(std_res_lm_agri_educ), min.n = 1)
+bwidth <- breaks[2]-breaks[1]
+qplot(std_res_lm_agri_educ, 
+      geom="histogram", 
+      binwidth = bwidth)
+ggplot(lm_agri_educ, 
+       aes(x=fit_res_lm_agri_educ, y=std_res_lm_agri_educ)) +
+  geom_point(shape=1) 
