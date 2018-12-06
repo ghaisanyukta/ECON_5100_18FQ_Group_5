50888e833e47ff9dd27552ce1e39cc1765ff059b
sourabh080892 <guptasourabh@seattleu.edu>
2018-12-06 12:56:45 -0800

    comments

--- code/06_regression_analysis.R
+++ code/06_regression_analysis.R
@@ -1,8 +1,8 @@
 lm_agri_educ <- lm(agri1c~highest_educ_level, data = agri_educ)
 
-res_lm_agri_educ <- resid(lm_agri_educ)
-std_res_lm_agri_educ <- rstandard(lm_agri_educ)
-fit_res_lm_agri_educ <- fitted(lm_agri_educ)
+res_lm_agri_educ <- resid(lm_agri_educ) # extract model residuals
+std_res_lm_agri_educ <- rstandard(lm_agri_educ) #compute regression
+fit_res_lm_agri_educ <- fitted(lm_agri_educ) # 
 
 breaks <- pretty(range(std_res_lm_agri_educ), n = nclass.FD(std_res_lm_agri_educ), min.n = 1)
 bwidth <- breaks[2]-breaks[1]
