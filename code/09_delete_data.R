de677702a34df5940321db075dde8a16f82a55ee
sanyuktaghai <sanyukta22@gmail.com>
2018-12-09 11:30:30 -0800

    Add delete raw data from workspace file

---
+++ code/09_delete_data.R
@@ -0,0 +1,6 @@
+#rm(list=ls())
+
+# Remove all raw data
+rm(sec0_data, sec1_data, sec2a_data, sec2b_data, sec2c_data, sec4a_data, sec4b_data,
+   sec4c_data, sec4d_data, sec4e_data, sec4f_data, sec4g_data, sec4h_data, sec4j_data,
+   sec5_data, sec6_data, sec7_data)
