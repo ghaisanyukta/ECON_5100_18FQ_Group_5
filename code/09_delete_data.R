0fc3b07c121f3ba8c1debd6eaeb39cdf2c675264
sanyuktaghai <sanyukta22@gmail.com>
2018-12-09 11:46:54 -0800

    Delete all raw_data

--- code/09_delete_data.R
+++ code/09_delete_data.R
@@ -1,6 +1,6 @@
 #rm(list=ls())
 
 # Remove all raw data
-rm(sec0_data, sec1_data, sec2a_data, sec2b_data, sec2c_data, sec4a_data, sec4b_data,
+rm(sec0a_data, sec1_data, sec2a_data, sec2b_data, sec2c_data, sec4a_data, sec4b_data,
    sec4c_data, sec4d_data, sec4e_data, sec4f_data, sec4g_data, sec4h_data, sec4j_data,
-   sec5_data, sec6_data, sec7_data)
+   sec5_data, sec6_data, sec7_data, sec8b_data, agg2_data, agg4_data)
