2ddfba6a14c7600d9defcad66e54116aeba51dd0
sanyuktaghai <sanyukta22@gmail.com>
2018-12-10 01:11:14 -0800

    Regression model

--- code/06_regression_analysis.R
+++ code/06_regression_analysis.R
@@ -1,14 +1,24 @@
-lm_agri_educ <- lm(agri1c~highest_educ_level, data = agri_educ)
+colnames(base)
+model1 <- lm(log(agri1c) ~ highest_educ_level
+             + hh_highest_educ + hh_is_male + ez 
+             + imputed_income + motorable_road_y + electricity_y + water_y
+             + public_transport_y + extension_centre_y + no_of_tractors
+             + rice_husking_machine_y + chemical_fertilizer_y + irrigated_fields_y
+             + region, data = base)
+summary(model1)
 
-res_lm_agri_educ <- resid(lm_agri_educ) # extract model residuals
-std_res_lm_agri_educ <- rstandard(lm_agri_educ) #compute regression
-fit_res_lm_agri_educ <- fitted(lm_agri_educ) # 
+res_model1 <- resid(model1) # extract model residuals
+std_res_model1 <- rstandard(model1) #compute regression
+fit_model1 <- fitted(model1) # 
 
-breaks <- pretty(range(std_res_lm_agri_educ), n = nclass.FD(std_res_lm_agri_educ), min.n = 1)
+breaks <- pretty(range(std_res_model1), n = nclass.FD(std_res_model1), min.n = 1)
 bwidth <- breaks[2]-breaks[1]
-qplot(std_res_lm_agri_educ, 
+qplot(std_res_model1, 
       geom="histogram", 
-      binwidth = bwidth)
-ggplot(lm_agri_educ, 
-       aes(x=fit_res_lm_agri_educ, y=std_res_lm_agri_educ)) +
+      binwidth = bwidth,
+      xlab = "standardized residuals",
+      ylab = "agricultural profit")
+#xlab("Fitted Residuals"), ylab("Standardized Residuals"),
+ggplot(model1, 
+       aes(x=fit_model1, y=std_res_model1)) +
   geom_point(shape=1) 
